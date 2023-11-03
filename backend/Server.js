161acf829cf0e6fb7d2c976acc5bb6c95f6beacb
Yigit Yaldiz <yigityaldiz@gmail.com>
2023-11-03 18:49:39 +0300

    fix port

--- backend/Server.js
+++ backend/Server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const router = require("./routes/TaskRoute");
 
 const app = express();
-const PORT = 3000;
+const PORT =  process.env.PORT ||  3000;
 
 app.use(express.json());
 app.use(cors());
