4df7895dbed339874179ecaa405472114437484e
Yigit Yaldiz <yigityaldiz@gmail.com>
2023-11-03 19:58:13 +0300

    test3

--- backend/Server.js
+++ backend/Server.js
@@ -11,7 +11,7 @@ const PORT =  process.env.PORT ||  3000;
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect(process.env.MONGODB_URI , {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
